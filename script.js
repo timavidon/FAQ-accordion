c74c77f5b6094cbfe5e5b329be2f31889ccb044c
Tim Avidon <timavidon@gmail.com>
2023-12-11 22:45:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ accordionHeaders.forEach((accordionHeader) => {
   accordionHeader.onclick = () => {
     let expanded =
       accordionHeader.getAttribute("aria-expanded") === "true" || false;
+    console.log(`from: ${expanded}`);
     accordionHeader.setAttribute("aria-expanded", !expanded);
     target.hidden = expanded;
+    console.log(`to: ${!expanded}`);
   };
 });
+
+// const accordionHeaders = document.querySelectorAll(".accordion-header");
+// accordionHeaders.forEach((accordionHeader) => {
+//   let target = accordionHeader.parentElement.nextElementSibling;
+//   accordionHeader.onclick = () => {
+
+//   };
+// });
